452fef7057968299672aba7ad230910799318ba0
jonjonathanarias <106196489+jonjonathanarias@users.noreply.github.com>
2026-09-01 21:10:20 -0300

    UI del Catálogo (lib/features/auth/presentation/pages/catalogo_page.dart

--- apps/mobile_app/lib/main.dart
+++ apps/mobile_app/lib/main.dart
@@ -8,6 +8,10 @@ import 'features/auth/presentation/bloc/auth_bloc.dart';
 import 'features/auth/presentation/bloc/auth_event.dart';
 import 'features/auth/presentation/pages/login_page.dart';
 
+import 'features/cliente/data/datasources/cliente_remote_datasource.dart';
+import 'features/cliente/data/repositories/cliente_repository_impl.dart';
+import 'features/cliente/presentation/bloc/catalogo_bloc.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -24,30 +28,46 @@ void main() {
   );
   final authRepository = AuthRepositoryImpl(remoteDataSource: authDataSource);
 
+  // Instancias para Cliente
+  final clienteDataSource = ClienteRemoteDataSourceImpl(dio: dioClient.dio);
+  final clienteRepository = ClienteRepositoryImpl(remoteDataSource: clienteDataSource);
+
   runApp(
     MyApp(
       authRepository: authRepository,
+      clienteRepository: clienteRepository,
       storage: storage,    ),
   );
 }
 
 class MyApp extends StatelessWidget {
   final AuthRepositoryImpl authRepository;
+  final ClienteRepositoryImpl clienteRepository;
   final FlutterSecureStorage storage;
 
   const MyApp({
     super.key,
     required this.authRepository,
+    required this.clienteRepository,
     required this.storage,
   });
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AuthBloc(
-        authRepository: authRepository,
-        storage: storage, // <-- Pases la instancia de storage requerida
-      )..add(AuthCheckRequested()), // <-- Verifica si ya existe token al iniciar
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AuthBloc(
+            authRepository: authRepository,
+            storage: storage,
+          )..add(AuthCheckRequested()),
+        ),
+        BlocProvider(
+          create: (context) => CatalogoBloc(
+            clienteRepository: clienteRepository,
+          ),
+        ),
+      ],
       child: MaterialApp(
         title: 'Puerta a Puerta',
         debugShowCheckedModeBanner: false,
